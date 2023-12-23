5a2dd07a705bca2341afcf5ffccca7da65e90b5e
luizfranciscodev <luizfranciscodev@gmail.com>
2023-12-23 19:08:59 -0300

    Adicionando Classe para melhor entendimento do codigo, e comentario no codigo para melhor entendimento.

--- index.php
+++ index.php
@@ -1,17 +1,51 @@
 <?php
-    require "src/conexao-bd.php";
+global $pdo;  // Declaração da variável $pdo como global
 
-    $sql1 = "SELECT * FROM produtos WHERE tipo = 'Café' ORDER BY 'preco'";
-    $statement = $pdo->query($sql1);
-    $produtosCafe = $statement->fetchAll(PDO::FETCH_ASSOC);
+// Inclui o arquivo que cria a instância do PDO
+require "src/conexao-bd.php";
+require "src/Modelo/Produto.php";
 
-    $sql2 = "SELECT * FROM produtos WHERE tipo = 'Almoço' ORDER BY 'preco'";
-    $statement = $pdo->query($sql2);
-    $produtosAlmoco = $statement->fetchAll(PDO::FETCH_ASSOC);
+// Consulta SQL para selecionar produtos do tipo 'Café' ordenados por preço
+$sql1 = "SELECT * FROM produtos WHERE tipo = 'Café' ORDER BY preco";
+
+// Executa a consulta SQL e obtém os resultados
+$statementCafe = $pdo->query($sql1);
+$produtosCafe = $statementCafe->fetchAll(PDO::FETCH_ASSOC);
+
+// Utiliza a função array_map para criar objetos da classe Produto para cada produto de café
+$dadosCafe = array_map(function ($cafe) {
+    // Cria uma nova instância da classe Produto para cada elemento do array
+    return new Produto(
+        $cafe['id'],         // Atribui o valor da chave 'id' do array $cafe à propriedade 'id' da instância
+        $cafe['tipo'],       // Atribui o valor da chave 'tipo' do array $cafe à propriedade 'tipo' da instância
+        $cafe['nome'],       // Atribui o valor da chave 'nome' do array $cafe à propriedade 'nome' da instância
+        $cafe['descricao'],  // Atribui o valor da chave 'descricao' do array $cafe à propriedade 'descricao' da instância
+        $cafe['imagem'],     // Atribui o valor da chave 'imagem' do array $cafe à propriedade 'imagem' da instância
+        $cafe['preco']       // Atribui o valor da chave 'preco' do array $cafe à propriedade 'preco' da instância
+    );
+}, $produtosCafe);  // O array $produtosCafe contém informações sobre os produtos do tipo café
 
 
+// Consulta SQL para selecionar produtos do tipo 'Almoço' ordenados por preço
+$sql2 = "SELECT * FROM produtos WHERE tipo = 'Almoço' ORDER BY preco";
+
+// Executa a consulta SQL e obtém os resultados
+$statementAlmoco = $pdo->query($sql2);
+$produtosAlmoco = $statementAlmoco->fetchAll(PDO::FETCH_ASSOC);
+
+// Utiliza a função array_map para criar objetos da classe Produto para cada produto de almoço
+$dadosAlmoco = array_map(function ($almoco) {
+    // Cria uma nova instância da classe Produto para cada elemento do array
+    return new Produto(
+        $almoco['id'],         // Atribui o valor da chave 'id' do array $almoco à propriedade 'id' da instância
+        $almoco['tipo'],       // Atribui o valor da chave 'tipo' do array $almoco à propriedade 'tipo' da instância
+        $almoco['nome'],       // Atribui o valor da chave 'nome' do array $almoco à propriedade 'nome' da instância
+        $almoco['descricao'],  // Atribui o valor da chave 'descricao' do array $almoco à propriedade 'descricao' da instância
+        $almoco['imagem'],     // Atribui o valor da chave 'imagem' do array $almoco à propriedade 'imagem' da instância
+        $almoco['preco']       // Atribui o valor da chave 'preco' do array $almoco à propriedade 'preco' da instância
+    );
+}, $produtosAlmoco);  // O array $produtosAlmoco contém informações sobre os produtos do tipo almoço
 ?>
-
 
 <!doctype html>
 <html lang="pt-br">
@@ -27,52 +61,67 @@
     <link rel="icon" href="img/icone-serenatto.png" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <title>Serenatto - Cardápio</title>
-</head>
+    <title>Serenatto - Cardápio</title></head>
 <body>
-    <main>
-        <section class="container-banner">
-            <div class="container-texto-banner">
-                <img src="img/logo-serenatto.png" class="logo" alt="logo-serenatto">
-            </div>
-        </section>
-        <h2>Cardápio Digital</h2>
-        <section class="container-cafe-manha">
-            <div class="container-cafe-manha-titulo">
-                <h3>Opções para o Café</h3>
-                <img class= "ornaments" src="img/ornaments-coffee.png" alt="ornaments">
-            </div>
-            <div class="container-cafe-manha-produtos">
-                <?php foreach ( $produtosCafe as $cafe):?>
+<main>
+    <!-- Seção do banner -->
+
+    <!-- Título principal -->
+    <h2>Cardápio Digital</h2>
+
+    <!-- Seção de produtos para o café da manhã -->
+    <section class="container-cafe-manha">
+        <!-- Título e ornamentação -->
+        <div class="container-cafe-manha-titulo">
+            <h3>Opções para o Café</h3>
+            <img class="ornaments" src="img/ornaments-coffee.png" alt="ornaments">
+        </div>
+
+        <!-- Container para os produtos de café -->
+        <div class="container-cafe-manha-produtos">
+            <?php foreach ($dadosCafe as $cafe): ?>
+                <!-- Container individual para cada produto de café -->
                 <div class="container-produto">
+                    <!-- Imagem do produto -->
                     <div class="container-foto">
-                        <img src="<?= "img/".$cafe['imagem']?>">
+                        <img src="<?= "img/" . $cafe->getImagem() ?>" alt="<?= $cafe->getDescricao() ?>">
                     </div>
-                    <p><?= $cafe['nome']?></p>
-                    <p><?= $cafe['descricao']?></p>
-                    <p><?= "R$ ".$cafe['preco']?></p>
+
+                    <!-- Nome, descrição e preço do produto -->
+                    <p><?= $cafe->getNome() ?></p>
+                    <p><?= $cafe->getDescricao() ?></p>
+                    <p><?= $cafe->getPrecoFormatado() ?></p>
                 </div>
-                <?php endforeach; ?>
-            </div>
-        </section>
-        <section class="container-almoco">
-            <div class="container-almoco-titulo">
-                <h3>Opções para o Almoço</h3>
-                <img class= "ornaments" src="img/ornaments-coffee.png" alt="ornaments">
-            </div>
-            <div class="container-almoco-produtos">
-                <?php foreach ($produtosAlmoco as $almoco):?>
+            <?php endforeach; ?>
+        </div>
+    </section>
+
+    <!-- Seção de produtos para o almoço -->
+    <section class="container-almoco">
+        <!-- Título e ornamentação -->
+        <div class="container-almoco-titulo">
+            <h3>Opções para o Almoço</h3>
+            <img class="ornaments" src="img/ornaments-coffee.png" alt="ornaments">
+        </div>
+
+        <!-- Container para os produtos de almoço -->
+        <div class="container-almoco-produtos">
+            <?php foreach ($dadosAlmoco as $almoco): ?>
+                <!-- Container individual para cada produto de almoço -->
                 <div class="container-produto">
+                    <!-- Imagem do produto -->
                     <div class="container-foto">
-                        <img src="<?= "img/".$almoco['imagem'] ?>">
+                        <img src="<?= "img/" . $almoco->getImagem() ?>" alt="<?= $almoco->getDescricao() ?>">
                     </div>
-                    <p><?= $almoco['nome'] ?></p>
-                    <p><?= $almoco['descricao'] ?></p>
-                    <p><?= "R$ ".$almoco['preco'] ?></p>
+
+                    <!-- Nome, descrição e preço do produto -->
+                    <p><?= $almoco->getNome() ?></p>
+                    <p><?= $almoco->getDescricao() ?></p>
+                    <p><?= $almoco->getPrecoFormatado() ?></p>
                 </div>
-                <?php endforeach; ?>
-            </div>
-        </section>
-    </main>
+            <?php endforeach; ?>
+        </div>
+    </section>
+</main>
 </body>
 </html>
--- src/conexao-bd.php
+++ src/conexao-bd.php
@@ -1,3 +1,19 @@
 <?php
+// conexao-bd.php
 
-$pdo = new PDO('mysql:host=localhost;dbname=serenatto', 'root', 'uphsuto123@KL');
+// Configurações do banco de dados
+$dsn = "mysql:host=localhost;dbname=serenatto";  // DSN (Data Source Name) para MySQL
+$usuario = "root";                              // Nome de usuário do banco de dados
+$senha = "uphsuto123@KL";                       // Senha do banco de dados
+
+try {
+    // Cria uma nova instância do PDO (PHP Data Objects)
+    $pdo = new PDO($dsn, $usuario, $senha);
+
+    // Configura o PDO para lançar exceções em caso de erros
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Captura erros de conexão e exibe uma mensagem de erro
+    echo 'Erro de conexão: ' . $e->getMessage();
+}
+
